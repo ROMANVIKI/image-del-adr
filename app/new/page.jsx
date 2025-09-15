0a2498e48919af4bfe3203f4e09d15909e5f93d2
ROMANVIKI <vikiadhi2016@gmail.com>
2025-09-15 13:39:04 +0530

    new page

---
+++ app/new/page.jsx
@@ -0,0 +1,16 @@
+const page = () => {
+  return (
+    <div>
+      <Image
+        className="dark:invert w-full"
+        src="/footer.png"
+        fill
+        alt="Next.js logo"
+        priority
+      />
+
+    </div>
+  )
+}
+
+export default page
